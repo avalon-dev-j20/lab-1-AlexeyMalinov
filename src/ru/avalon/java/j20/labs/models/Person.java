ff081a67fd5b4cfaf8f8ded8ec76f3a16e388711
AlexeyMalinov <alexeymalinov@yandex.ru>
2019-04-11 15:02:39 +0300

    completed Task4

--- src/ru/avalon/java/j20/labs/models/Person.java
+++ src/ru/avalon/java/j20/labs/models/Person.java
@@ -1,5 +1,7 @@
 package ru.avalon.java.j20.labs.models;
 
+import java.util.Objects;
+
 /**
  * Представление о человеке.
  */
@@ -17,7 +19,7 @@ public class Person {
      * Создаёт экземпляр класса на основании имени и даты
      * рождения.
      *
-     * @param name имя человека
+     * @param name    имя человека
      * @param surname дата рождения человека
      */
     public Person(String name, String surname) {
@@ -42,4 +44,25 @@ public class Person {
     public String getSurname() {
         return surname;
     }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param object
+     * @return
+     */
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof Person) {
+            Person other = (Person) object;
+            return this.name.equals(other.name) && this.surname.equals(other.surname);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
 }
